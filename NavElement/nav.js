0619acf56c4a826774ec9295164561dcf5e6b303
EmmaAmos <elovestodraw@gmail.com>
2025-03-24 13:20:46 -0400

    fixing nav

--- NavElement/nav.js
+++ NavElement/nav.js
@@ -1,4 +1,6 @@
 //Nav Fetch
+
+/*
     fetch('/Nav.html')
         .then(response => response.text())
         .then(data => {
@@ -15,3 +17,38 @@
         .then(data => {
             document.getElementById('endCard').innerHTML = data;
         });
+*/
+
+    
+    fetch('/Nav.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('nav-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Fetch error:', error));
+
+
+    fetch('/Headline.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('headline').innerHTML = data;
+    })
+    .catch(error => console.error('Fetch error:', error));
+
+    
+
+    fetch('/Social.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Network response was not ok');
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('endCard').innerHTML = data;
+    })
+    .catch(error => console.error('Fetch error:', error));
